b987b96fd7b15ef5a76d17aad4fcce7cafe365bc
Venky <vgopal3@jhu.edu>
2017-09-12 15:31:53 -0400

    First commit

---
+++ vigenere_encrypt.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+  "fmt"
+  "io/ioutil"
+  "log"
+  "strings"
+  "os"
+  "regexp"
+  "bytes"
+)
+
+func main() {
+
+  if (len(os.Args[1:]) != 2) {
+    fmt.Println(" Use the command line specification mentioned in the Assignment\n <executable_name> <keys_file_name> <plaintext_file_name> \n")
+
+  } else {
+
+  key := os.Args[1]
+  if (len(key) > 32) {
+    fmt.Println(" Invalid key length \n Key length should be upto 32 characters \n")
+  }
+  file_name := os.Args[2]
+  file, err_data_file := ioutil.ReadFile(file_name)
+  /* Error handling if file wasn't opened successfully */
+  if (err_data_file != nil) {
+    log.Fatal(err_data_file)
+  }
+  /* Need to perform below operation as ReadFile() returns a byte array */
+  str := strings.ToUpper(string(file))
+  re := regexp.MustCompile("[^[:alpha:]]")
+  s := re.ReplaceAllLiteralString(str,"")
+
+  vignere_encryption(s,key)
+
+}
+
+}
+
+func vignere_encryption(str string, key string) {
+	// Define an array to hold ascii characters belonging to the key
+	var key_ascii []int
+	key_length := len(key)
+	key_ascii = make([]int, key_length, key_length)
+	for j:=0; j < key_length; j++ {
+		key_ascii[j] = int(key[j])
+	}
+
+	// Define an array to hold ascii characters belonging to the plaintext
+	var ascii []int
+	str_length := len(str)
+	ascii = make([]int, str_length, str_length)
+	for i:=0 ; i < str_length; i++ {
+		for z:=0; z < key_length; z++ {
+
+			/* Logic here is I try all commbinations and check if the mod of the
+			 index in the plaintext matches the index for the key. If yes, I encode
+			 that character */
+			if ((i % key_length) == z) {
+			ascii[i] = (((int(str[i]) - 65) + (key_ascii[z] -65)) % 26) + 65
+		}
+		}
+	}
+
+
+	// Convert the ascii array back to a string and write to a buffer
+	var buffer bytes.Buffer
+	for k:= 0; k < len(ascii); k++ {
+			buffer.WriteString(string(ascii[k]))
+	}
+	fmt.Println(buffer.String())
+
+}
---
+++ vigenere_keylength.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+  "fmt"
+  "io/ioutil"
+  "log"
+  "strings"
+  "os"
+  "regexp"
+  "sort"
+  //"bytes"
+)
+
+func main() {
+
+  if (len(os.Args[1:]) != 1) {
+    fmt.Println(" Use the command line specification mentioned in the Assignment\n <executable_name> <ciphertext_file_name> \n")
+  } else {
+    file_name := os.Args[1]
+    file, err_data_file := ioutil.ReadFile(file_name)
+    /* Error handling if file wasn't opened successfully */
+    if (err_data_file != nil) {
+      log.Fatal(err_data_file)
+    }
+    str := strings.ToUpper(string(file))
+    re := regexp.MustCompile("[^[:alpha:]]")
+    s := re.ReplaceAllLiteralString(str,"")
+
+    FindKeyLength(s)
+  }
+}
+
+func FindKeyLength(str string) {
+
+  str_length := len(str)
+  var counter []int
+  maximum_key_size := 100
+  counter = make([]int, maximum_key_size, maximum_key_size)
+
+  for shift := 1; shift < maximum_key_size; shift++ {
+
+    for i := 0; i < str_length ; i++ {
+
+    // Case when we want to ignore the characters past the string length
+    // post shifting
+      if((i + shift) >= str_length) {
+        break
+      }
+    c := string(str[i])
+    d := string(str[i + shift])
+
+    if(c == d) {
+      counter[shift] += 1
+    }
+  }
+
+}
+  /*for a:=0; a< maximum_key_size; a++ {
+    fmt.Println(a , " : ", counter[a])
+
+  } */
+
+  // Define a slice for having a new copy of array to sort
+  var sorted_counter []int
+  counter_length := len(counter)
+  sorted_counter = make([]int, counter_length, counter_length)
+
+  for i := 0; i < counter_length; i++ {
+    sorted_counter[i] = counter[i]
+  }
+  sort.Ints(sorted_counter)
+  //fmt.Println(sorted_counter)
+
+  // Maintain the indices which have the maximum value
+  var index [4]int
+  for i:=0; i < 4; i++ {
+    index[i] = findIndex(counter, counter_length, sorted_counter[counter_length -(i + 1)])
+}
+  fmt.Println(" The indices are ", index)
+  gcd_1 := gcd(index[0], index[1])
+  gcd_2 := gcd(index[2], index[3])
+  predicted_key_length := gcd(gcd_1, gcd_2)
+  fmt.Println( " Predicted key lenth is ", predicted_key_length)
+}
+
+func findIndex(counter []int, length int, value int) int {
+  i := 0
+  for i = 0; i < length; i++ {
+    if(counter[i] == value) {
+      return i
+    }
+  }
+  return i
+  }
+
+func gcd(temp1 int,temp2 int) (int){
+       var gcdnum int
+    /* Use of And operator in For Loop */
+    for i := 1; i <=temp1 && i <=temp2 ; i++ {
+            if(temp1%i==0 && temp2%i==0) {
+                gcdnum=i
+            }
+        }
+        return gcdnum
+}
